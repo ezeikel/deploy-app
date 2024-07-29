f6618398a4aec24969eae7cb58b8432e9cedb957
Ezeikel Pemberton <hi@ezeikel.dev>
2024-07-29 15:11:36 +0100

    feat: example app.

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+
+  return (
+    <div className="container">
+      <h1 className="title">Deploy me 👷🏿‍♂️</h1>
+      <img src="/badboys.webp" />
+    </div>
+  )
+}
+
+export default App
